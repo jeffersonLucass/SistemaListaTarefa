6426ec4fe8cdd3c09d3d71782cb28fd93e062385
jeffersonLucass <jeffersonlucas810@gmail.com>
2024-11-08 11:43:01 -0300

    adicionei algumas validações

--- models/tarefa.js
+++ models/tarefa.js
@@ -8,19 +8,33 @@ Tarefa.init({
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
+        validate: {
+            notEmpty: true, // Validação para não aceitar valores vazios
+        }
     },
     custo: {
         type: DataTypes.DECIMAL(10, 2),
         allowNull: false,
+        validate: {
+            isDecimal: true, // Validação para aceitar apenas números decimais
+            min: 0,          // Validação para garantir valor positivo
+        }
     },
     data_limite: {
         type: DataTypes.DATE,
         allowNull: false,
+        validate: {
+            isDate: true,    // Validação para garantir que seja uma data válida
+        }
     },
     ordem_apresentacao: {
         type: DataTypes.INTEGER,
         allowNull: false,
         unique: true,
+        validate: {
+            isInt: true,     // Validação para garantir que seja um número inteiro
+            min: 1           // Definir um valor mínimo para a ordem (opcional)
+        }
     },
 }, {
     sequelize,
